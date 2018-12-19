fa789b1b08e86a5a6ebde69380a7336330cb4a30
samuraikun <tazitaziawawa@gmail.com>
2018-12-19 21:38:45 +0900

    async fetch

--- src/api/member/index.ts
+++ src/api/member/index.ts
@@ -1,8 +1,25 @@
 import { MemberEntity } from '../../model';
-import { members } from './mockData';
 
-const fetchMembers = (): Promise<MemberEntity[]> => {
-  return Promise.resolve(members);
+const baseURL = 'https://api.github.com/orgs/lemoncode';
+
+const fetchMembers = async (): Promise<MemberEntity[]> => {
+  const membersURL = `${baseURL}/members`;
+  const res = await fetch(membersURL);
+  const members = await res.json();
+
+  return mapToMembers(members);
+};
+
+const mapToMembers = (githubMembers: any[]): MemberEntity[] => {
+  return githubMembers.map(mapToMember);
+};
+
+const mapToMember = (githubMember: MemberEntity) => {
+  return {
+    id: githubMember.id,
+    login: githubMember.login,
+    avatar_url: githubMember.avatar_url
+  };
 };
 
 export const memberAPI = {
